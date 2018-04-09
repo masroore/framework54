cabef02875e3cbbc29271270db6ff788e83b225d
Dr. Masroor Ehsan <masroore@gmail.com>
2018-04-09 22:05:46 +0200

    added blank() helper

--- src/Illuminate/Support/helpers.php
+++ src/Illuminate/Support/helpers.php
@@ -330,6 +330,35 @@ if (!function_exists('array_wrap')) {
     }
 }
 
+if (! function_exists('blank')) {
+    /**
+     * Determine if the given value is "blank".
+     *
+     * @param  mixed  $value
+     * @return bool
+     */
+    function blank($value)
+    {
+        if (is_null($value)) {
+            return true;
+        }
+
+        if (is_string($value)) {
+            return trim($value) === '';
+        }
+
+        if (is_numeric($value) || is_bool($value)) {
+            return false;
+        }
+
+        if ($value instanceof Countable) {
+            return count($value) === 0;
+        }
+
+        return empty($value);
+    }
+}
+
 if (!function_exists('camel_case')) {
     /**
      * Convert a value to camel case.
